be284c61513659ea20728b40275421f30d1baac2
tolyareg <86396475+tolyareg@users.noreply.github.com>
2025-05-19 12:26:05 +0500

    Update 2005.cpp

--- 2005.cpp
+++ 2005.cpp
@@ -1,130 +1,56 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <set>
+#include <limits> 
 
 using namespace std;
 
-bool check(long long min_coolness, int n, int m, const vector<long long>& v_pop, const vector<long long>& h_pop) {
-    
-    vector<bool> dp_curr(n, false);
-    vector<bool> dp_prev(n, false);
-
-    if (v_pop.empty() && h_pop.empty()) return true; 
-
-
-    dp_prev[0] = true; 
-    if (h_pop[0] >= min_coolness) {
-        for (int c = 0; c < n; ++c) {
-            dp_prev[c] = true;
-        }
-    }
-
-    if (m == 1) {
-        return dp_prev[n-1];
-    }
-
-    for (int r = 1; r < m; ++r) {
-        dp_curr.assign(n, false); 
-
-        if (dp_prev[0] && (v_pop[0] >= min_coolness)) {
-            dp_curr[0] = true;
-        }
-
-        bool H_r_usable = (h_pop[r] >= min_coolness);
-        for (int c = 1; c < n; ++c) {
-            bool can_reach_from_up = dp_prev[c] && (v_pop[c] >= min_coolness);
-            bool can_reach_from_left = false;
-            if (H_r_usable) {
-                can_reach_from_left = dp_curr[c-1];
-            }
-            dp_curr[c] = can_reach_from_up || can_reach_from_left;
-        }
-        dp_prev = dp_curr; 
-    }
-
-    return dp_prev[n-1]; 
-}
-
-bool check_corrected(long long min_coolness, int n, int m, const vector<long long>& v_pop, const vector<long long>& h_pop) {
-    vector<bool> dp_row(n, false);
-
-    dp_row[0] = true; 
-
-
-    if (h_pop[0] >= min_coolness) {
-        for (int c = 1; c < n; ++c) {
-            dp_row[c] = dp_row[c-1];
-        }
-    } else { 
-        for (int c = 1; c < n; ++c) {
-            dp_row[c] = false;
-        }
-    }
-
-
-    for (int r = 1; r < m; ++r) {
-        if (v_pop[0] >= min_coolness) {
-            // dummy
-        } else {
-            dp_row[0] = false; 
-        }
-        
-
-        bool H_r_usable = (h_pop[r] >= min_coolness);
-        for (int c = 1; c < n; ++c) {
-
-            bool can_reach_from_up = dp_row[c] && (v_pop[c] >= min_coolness);
-            bool can_reach_from_left = false;
-            if (H_r_usable) {
-                can_reach_from_left = dp_row[c-1]; 
-            }
-            dp_row[c] = can_reach_from_up || can_reach_from_left;
-        }
-    }
-    return dp_row[n-1];
-}
-
-
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    int D[5][5];
 
-    int n_vertical_streets, m_horizontal_streets;
-    cin >> n_vertical_streets >> m_horizontal_streets;
-
-    vector<long long> v_pop(n_vertical_streets);
-    set<long long> distinct_pops;
-
-    for (int i = 0; i < n_vertical_streets; ++i) {
-        cin >> v_pop[i];
-        distinct_pops.insert(v_pop[i]);
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 5; ++j) {
+            cin >> D[i][j];
+        }
     }
 
-    vector<long long> h_pop(m_horizontal_streets);
-    for (int i = 0; i < m_horizontal_streets; ++i) {
-        cin >> h_pop[i];
-        distinct_pops.insert(h_pop[i]);
-    }
-    
-    long long low = 1, high = 1000000000LL;
-    long long ans = 0;
+    vector<int> students = {2, 3, 4}; 
+    sort(students.begin(), students.end());
 
-    while(low <= high) {
-        long long mid = low + (high - low) / 2;
-        if (mid == 0) {
-            low = 1;
+    long long min_total_dist = numeric_limits<long long>::max();
+    vector<int> optimal_path_points;
+
+    do {
+        int s1 = students[0];
+        int s2 = students[1];
+        int s3 = students[2];
+
+        if (s3 == 3) {
             continue;
         }
-        if (check_corrected(mid, n_vertical_streets, m_horizontal_streets, v_pop, h_pop)) {
-            ans = mid;
-            low = mid + 1;
-        } else {
-            high = mid - 1;
+
+        vector<int> current_path_candidate = {1, s1, s2, s3, 5};
+        long long current_path_total_dist = 0;
+
+
+        current_path_total_dist += D[current_path_candidate[0]-1][current_path_candidate[1]-1];
+        current_path_total_dist += D[current_path_candidate[1]-1][current_path_candidate[2]-1];
+        current_path_total_dist += D[current_path_candidate[2]-1][current_path_candidate[3]-1];
+        current_path_total_dist += D[current_path_candidate[3]-1][current_path_candidate[4]-1];
+        
+        if (current_path_total_dist < min_total_dist) {
+            min_total_dist = current_path_total_dist;
+            optimal_path_points = current_path_candidate;
         }
+
+    } while (next_permutation(students.begin(), students.end()));
+
+    cout << min_total_dist << endl;
+
+    for (size_t i = 0; i < optimal_path_points.size(); ++i) {
+        cout << optimal_path_points[i] << (i == optimal_path_points.size() - 1 ? "" : " ");
     }
-    
-    cout << ans << endl;
+    cout << endl;
 
     return 0;
 }
